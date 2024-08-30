d2386e26f07ea4e838af72af1ac149e344693c55
jaydipsonani <132323340+jaydipsonani@users.noreply.github.com>
2024-08-30 10:48:15 +0530

    Update eSIMChecker.js

--- src/eSIMChecker.js
+++ src/eSIMChecker.js
@@ -339,7 +339,7 @@ const detectEsimSupport = () => {
       const version = parseFloat(androidVersionMatch[1]);
       
       // Check if the Android version is 12 or above
-      if (version >= 11) {
+      if (version >= 10) {
         esimSupported = true;
       }
 
