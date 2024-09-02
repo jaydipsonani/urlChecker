3d9c6ffa02f679c344332ecf7899e027eef154c3
jaydipsonani <132323340+jaydipsonani@users.noreply.github.com>
2024-09-02 11:29:00 +0530

    Update eSIMChecker.js

--- src/eSIMChecker.js
+++ src/eSIMChecker.js
@@ -8,8 +8,8 @@ const detectEsimSupport = () => {
   const getIosDeviceName = () => {
     // Mapping internal identifiers to device names
     const iosDevices = {
-      'iPhone10,1': 'iPhone 8',
-      'iPhone10,2': 'iPhone 8 Plus',
+      // 'iPhone10,1': 'iPhone 8',
+      // 'iPhone10,2': 'iPhone 8 Plus',
       'iPhone10,3': 'iPhone X',
       'iPhone10,6': 'iPhone X',
       'iPhone11,2': 'iPhone XS',
@@ -42,7 +42,7 @@ const detectEsimSupport = () => {
       const platform = navigator.platform;
       if (platform.includes('iPhone')) {
 
-        const deviceModel = 'iPhone10,2' || 'iPhone10,3' || 'iPhone10,6' || 'iPhone11,2' || 'iPhone11,4'
+        const deviceModel = 'iPhone10,3' || 'iPhone10,6' || 'iPhone11,2' || 'iPhone11,4'
         || 'iPhone11,6' || 'iPhone11,8' || 'iPhone12,1' || 'iPhone12,3' || 'iPhone12,5' || 'iPhone12,8'
         || 'iPhone13,1' || 'iPhone13,2' || 'iPhone13,3' || 'iPhone13,4' || 'iPhone14,4' || 'iPhone14,5'
         || 'iPhone14,2' || 'iPhone14,3' || 'iPhone14,7' || 'iPhone14,8' || 'iPhone15,2' || 'iPhone15,3'; 
@@ -57,7 +57,7 @@ const detectEsimSupport = () => {
   if (/iPhone|iPad|iPod/i.test(userAgent)) {
     const deviceName = getIosDeviceName();
     return {
-      isSupported: ['iPhone XS', 'iPhone XS Max', 'iPhone XR', 'iPhone 11', 'iPhone 11 Pro', 'iPhone 11 Pro Max', 'iPhone SE (2nd generation)', 'iPhone 12', 'iPhone 12 mini', 'iPhone 12 Pro', 'iPhone 12 Pro Max', 'iPhone 13', 'iPhone 13 mini', 'iPhone 13 Pro', 'iPhone 13 Pro Max', 'iPhone 14', 'iPhone 14 Plus', 'iPhone 14 Pro', 'iPhone 14 Pro Max', 'iPhone 8 Plus'].includes(deviceName),
+      isSupported: ['iPhone XS', 'iPhone XS Max', 'iPhone XR', 'iPhone 11', 'iPhone 11 Pro', 'iPhone 11 Pro Max', 'iPhone SE (2nd generation)', 'iPhone 12', 'iPhone 12 mini', 'iPhone 12 Pro', 'iPhone 12 Pro Max', 'iPhone 13', 'iPhone 13 mini', 'iPhone 13 Pro', 'iPhone 13 Pro Max', 'iPhone 14', 'iPhone 14 Plus', 'iPhone 14 Pro', 'iPhone 14 Pro Max'].includes(deviceName),
       deviceName
     };
   }
