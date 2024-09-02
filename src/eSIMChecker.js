3a6ffe327acee7b1663f18cd1584d4a599069f8c
jaydipsonani <132323340+jaydipsonani@users.noreply.github.com>
2024-09-02 16:00:33 +0530

    Update eSIMChecker.js

--- src/eSIMChecker.js
+++ src/eSIMChecker.js
@@ -27,7 +27,7 @@ const detectEsimSupport = () => {
     // { pattern: /iPhone.*OS 12_0/, model: 'iPhone XS' },
     // { pattern: /iPhone.*OS 11_0/, model: 'iPhone 8 Plus' }, 
 
-  { pattern: /iPhone.*OS 16_1/, model: 'iPhone 8 Plus Plus' }, // Latest version for iPhone 8 Plus
+  { pattern: /iPhone.*OS 16_1/, model: 'iPhone 8 Plus' }, // Latest version for iPhone 8 Plus
   { pattern: /iPhone.*OS 16_7/, model: 'iPhone 11/11 Pro/11 Pro Max' },// Latest version for iPhone 11 series
   { pattern: /iPhone.*OS 17_0/, model: 'iPhone 12/12 mini/12 Pro/12 Pro Max' },
   { pattern: /iPhone.*OS 17_0/, model: 'iPhone 13/13 mini/13 Pro/13 Pro Max' },
@@ -63,7 +63,7 @@ const detectEsimSupport = () => {
         'iPhone 11/11 Pro/11 Pro Max', 'iPhone 12/12 mini/12 Pro/12 Pro Max',
         'iPhone 13/13 mini/13 Pro/13 Pro Max','iPhone SE (2nd generation)',
         'iPhone 14/14 Plus/14 Pro/14 Pro Max','iPhone 15/15 Plus/15 Pro/15 Pro Max',
-        'iPhone XS/XS Max/XR','iPhone 8 Plus Plus'
+        'iPhone XS/XS Max/XR','iPhone 8 Plus'
       ].includes(deviceName),
       deviceName
     };
