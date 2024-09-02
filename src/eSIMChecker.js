113e4666ce539889f5886af6c848acdad9461e83
jaydipsonani <132323340+jaydipsonani@users.noreply.github.com>
2024-09-02 12:34:00 +0530

    Update eSIMChecker.js

--- src/eSIMChecker.js
+++ src/eSIMChecker.js
@@ -55,7 +55,7 @@ const detectEsimSupport = () => {
         'iPhone XS', 'iPhone XS Max', 'iPhone XR', 'iPhone 11', 'iPhone 11 Pro', 'iPhone 11 Pro Max',
         'iPhone SE (2nd generation)', 'iPhone 12', 'iPhone 12 mini', 'iPhone 12 Pro', 'iPhone 12 Pro Max',
         'iPhone 13', 'iPhone 13 mini', 'iPhone 13 Pro', 'iPhone 13 Pro Max', 'iPhone 14', 'iPhone 14 Plus',
-        'iPhone 14 Pro', 'iPhone 14 Pro Max', 'iPhone 8 Plus'
+        'iPhone 14 Pro', 'iPhone 14 Pro Max', 'iPhone 8 Plus', 'iPhone'
       ].includes(deviceName),
       deviceName
     };
